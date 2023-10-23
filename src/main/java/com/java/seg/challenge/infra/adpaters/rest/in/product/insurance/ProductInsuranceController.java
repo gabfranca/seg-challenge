d626c14d6fc060e80a1d947be29f1286ed84bf0a
gabfranca <gabriel_franca@outlook.com.br>
2023-10-23 01:00:59 -0300

    fix

--- src/main/java/com/java/seg/challenge/infra/adpaters/rest/in/product/insurance/ProductInsuranceController.java
+++ src/main/java/com/java/seg/challenge/infra/adpaters/rest/in/product/insurance/ProductInsuranceController.java
@@ -1,13 +1,11 @@
 package com.java.seg.challenge.infra.adpaters.rest.in.product.insurance;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.seg.challenge.core.model.Insurance;
